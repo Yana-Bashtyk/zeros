d04494599388c84726940b7bddc9bfa01590703d
Yana Bashtyk <Yana_bashtyk@epam.com>
2018-09-23 21:52:30 +0300

    solution zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
+  var zeroCounter = 0;
+  while (number = Math.floor(number/5)){
+    zeroCounter += number;
+  }
+  return zeroCounter;
   // your implementation
 }
